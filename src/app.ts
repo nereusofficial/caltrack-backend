c0af3e971923be7539803cb39102f8094859cb30
Daevid Lawrence Leonem Esporlas <esporlasdaevidlawrence.dost@gmail.com>
2026-06-06 14:31:14 +0800

    fsafsa

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,12 @@ const app: Application = express();
 app.use(helmet());
 
 // Enable CORS
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 // Request logging
 app.use(morgan("dev"));
