ce42bdbba756d60f83a9248783ef36431cd7412b
Daevid Lawrence Leonem Esporlas <esporlasdaevidlawrence.dost@gmail.com>
2026-06-06 13:50:59 +0800

    fgsafa

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,17 @@ dotenv.config();
 import "./config/db";
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+// ONLY LOCAL DEVELOPMENT
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`
+  app.listen(PORT, () => {
+    console.log(`
 ====================================
 🚀 CalTrack API running
 📍 Environment: ${process.env.NODE_ENV || "development"}
 🌐 Port: ${PORT}
 ====================================
-  `);
-});
+    `);
+  });
+}
